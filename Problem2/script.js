edb8024147d29b41e12926b27b96903e7c12e045
Cormac McCarthy <72230626+cmcc15@users.noreply.github.com>
2023-10-10 22:50:16 +0100

    Update script.js

--- Problem2/script.js
+++ Problem2/script.js
@@ -5,22 +5,17 @@ closeIcon = popupBox.querySelector("header i"),
 titleTag = popupBox.querySelector("input"),
 descTag = popupBox.querySelector("textarea"),
 addBtn = popupBox.querySelector("button");
-
 const months = ["January", "February", "March", "April", "May", "June", "July",
               "August", "September", "October", "November", "December"];
-			  
 const notes = JSON.parse(localStorage.getItem("notes") || "[]");
-
 let isUpdate = false, updateId;
-
 addBox.addEventListener("click", () => {
-    popupTitle.innerText = "Add Note";
-    addBtn.innerText = "Add a note";
+    popupTitle.innerText = "Add a new Note";
+    addBtn.innerText = "Add Note";
     popupBox.classList.add("show");
     document.querySelector("body").style.overflow = "hidden";
     if(window.innerWidth > 660) titleTag.focus();
 });
-
 closeIcon.addEventListener("click", () => {
     isUpdate = false;
     titleTag.value = descTag.value = "";
@@ -52,7 +47,7 @@ function showNotes() {
         addBox.insertAdjacentHTML("afterend", liTag);
     });
 }
-
+		
 showNotes();
 function showMenu(elem) {
     elem.parentElement.classList.add("show");
@@ -60,17 +55,19 @@ function showMenu(elem) {
         if(e.target.tagName != "I" || e.target != elem) {
             elem.parentElement.classList.remove("show");
         }
+		function changecolor(){
+			let color = document.getElementById('colorpicker').value;
+			document.body.style.backgroundColor = color; 
+		}
     });
 }
-
 function deleteNote(noteId) {
-    let confirmDel = confirm("You cannot get this back after you deleter this. Are you sure you want to delete?");
+    let confirmDel = confirm("Are you sure you want to delete this note?");
     if(!confirmDel) return;
     notes.splice(noteId, 1);
     localStorage.setItem("notes", JSON.stringify(notes));
     showNotes();
 }
-
 function updateNote(noteId, title, filterDesc) {
     let description = filterDesc.replaceAll('<br/>', '\r\n');
     updateId = noteId;
@@ -81,7 +78,6 @@ function updateNote(noteId, title, filterDesc) {
     popupTitle.innerText = "Update a Note";
     addBtn.innerText = "Update Note";
 }
-
 addBtn.addEventListener("click", e => {
     e.preventDefault();
     let title = titleTag.value.trim(),
@@ -103,3 +99,8 @@ addBtn.addEventListener("click", e => {
         closeIcon.click();
     }
 });
+
+function changecolor(){
+			let color = document.getElementById('colorpicker').value;
+			document.body.style.backgroundColor = color; 
+		}
