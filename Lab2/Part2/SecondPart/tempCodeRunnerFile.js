40a670af600dfff6486d4049c0e4ad02e21b13fd
Cormac McCarthy <cormacredmen@gmail.com>
2023-10-29 01:21:58 +0100

    Add files via upload

---
+++ Lab2/Part2/SecondPart/tempCodeRunnerFile.js
@@ -0,0 +1,19 @@
+import fetch from 'node-fetch';
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(posts => {
+    const wordFrequencyMap = posts
+      .map(post => post.body)
+      .join(' ')
+      .toLowerCase()
+      .split(/\W+/)
+      .filter(word => word.length > 0)
+      .reduce((freqMap, word) => {
+        freqMap[word] = (freqMap[word] || 0) + 1;
+        return freqMap;
+      }, {});
+
+    console.log(wordFrequencyMap);
+  })
+  .catch(error => console.error(error));
